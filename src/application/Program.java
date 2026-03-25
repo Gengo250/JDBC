6a04fba52dcafd2e4d0e1658c0f01158772a4bfe
Gengo250 <miguelgengo28@gmail.com>
2026-03-24 22:51:48 -0300

    create class department

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import db.DB;
 import db.DBIntegridyException;
+import model.entities.Department;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -11,32 +12,8 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Connection conn = null;
-        PreparedStatement st = null;
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
 
-        try {
-            conn = DB.getConnection();
-
-            st = conn.prepareStatement(
-                    "DELETE FROM department " +
-                            "WHERE Id = ?"
-            );
-
-            st.setInt(1, 5);
-
-            int rowsAffected = st.executeUpdate();
-
-            if (rowsAffected > 0) {
-                System.out.println("Done! " + rowsAffected + " row(s) affected");
-            } else {
-                System.out.println("No rows affected");
-            }
-
-        } catch (SQLException e) {
-            throw new DBIntegridyException(e.getMessage());
-        } finally {
-            DB.closeStatement(st);
-            DB.closeConnection();
-        }
     }
 }
